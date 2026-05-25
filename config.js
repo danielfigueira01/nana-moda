cb6b4a239f1b22ba2227a90b5f014e0cbcb3589a
danielfigueira01 <daniel.figueira.01@gmail.com>
2026-05-24 22:06:58 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.NANA_CONFIG = {
-  supabaseUrl: "https://ofarjltaoohnfyxbnuxx.supabase.co",
-  supabaseAnonKey: "sb_publishable_mdKAWCH7l-dwqUdYmbK1Yw_ziDlr89Y",
-  whatsappNumber: "5522988524928",
-  storeName: "Nana Moda Íntima",
-  city: "Nova Friburgo - RJ"
-};
